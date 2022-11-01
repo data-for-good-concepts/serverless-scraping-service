311b0393ab27ba78402a326c7f6613c29a06d087
Jeniffen Chandrabalan <c.jeniffen@gmail.com>
2022-11-01 00:32:07 -0400

    Fix github action cloud run yaml

--- global.R
+++ global.R
@@ -2,6 +2,10 @@
 
 source("packages.R")
 
+# Get environment variables -----------------------------------------------
+
+PORT <- Sys.getenv('PORT')
+
 # Initialise functions ----------------------------------------------------
 
 list.files("R", full.names = TRUE, pattern = "\\.R$") %>%
